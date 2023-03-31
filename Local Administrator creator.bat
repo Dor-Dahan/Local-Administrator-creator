368213a1d3df37a4fb3f47f87bb7362c1749de35
dor <32441786+Dor-Dahan@users.noreply.github.com>
2023-04-01 00:15:54 +0300

    Update Local Administrator creator.bat

--- Local Administrator creator.bat
+++ Local Administrator creator.bat
@@ -3,24 +3,27 @@ title Local Administrator creator ©made by dor©
 color a
 set /p localuser="local user name = "
 set /p localuserpass="local user pass = "
-echo "creating mitkan"
+
+echo "creating %local user%"
 net user %localuser% /ACTIVE:yes
 net user %localuser% %localuserpass% /y /add /PASSWORDREQ:NO /PASSWORDCHG:NO
 WMIC USERACCOUNT WHERE Name='%localuser%' SET PasswordExpires=false
 net localgroup administrators %localuser% /add
-echo "finish creating mitkan"
-echo "creating mitkan1"
+echo "finish creating %local user%"
+
+echo "creating %local user%1"
 net user %localuser%1 /active:yes
 net user %localuser%1 %localuserpass% /y /add /PASSWORDREQ:NO /PASSWORDCHG:NO
 WMIC USERACCOUNT WHERE Name='%localuser%1' SET PasswordExpires=false
 net localgroup administrators %localuser%1 /add
-echo "finish creating mitkan"
-echo "creating mitkan2"
+echo "finish creating %local user%1"
+
+echo "creating %local user%2"
 net user %localuser%2 /active:yes
 net user %localuser%2 %localuserpass% /y /add /PASSWORDREQ:NO /PASSWORDCHG:NO
 WMIC USERACCOUNT WHERE Name='%localuser%2' SET PasswordExpires=false
 net localgroup administrators %localuser%2 /add
-echo "finish creating mitkan"
+echo "finish creating %local user%"
 echo.
 echo Finished operation.
 timeout 5
